a75c4c15bde682b94026d791a44ce7d7125cd2b7
6KmG <117772085+6KmG@users.noreply.github.com>
2023-01-22 18:45:11 +0100

    C (single core)

--- benchmark.c
+++ benchmark.c
@@ -1,15 +1,16 @@
 //Compiled with gcc
 #include <math.h>
-
 int main(){
     int count = 1;
     int i;
     int j;
     char is_prime;
+    int square;
 
-    for (i=3;i<10000000;i+=2){
+    for (i=3;i<50000000;i+=2){
         is_prime = 1;
-        for (j = 3; j<=(sqrt(i)); j+=2){
+        square = sqrt(i);
+        for (j = 3; j<=square; j+=2){
             if (i%j==0){
                 is_prime = 0;
                 break;
